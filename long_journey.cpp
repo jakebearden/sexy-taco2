167c02d1b1a66047e0b953807a9d2e6fb09c7e5c
jakebearden <112139623+jakebearden@users.noreply.github.com>
2022-09-01 09:28:56 -0700

    Add files via upload

---
+++ long_journey.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+    int miles;
+    int money;
+    int days;
+
+    miles = 4328;
+    money = -30;
+    days = 19;
+
+    cout << "we have made a long journey of " << miles << " miles." << endl;
+    cout << "our checking balance is " << money << "." << endl;
+    cout << "about " << days << " years ago columbus stood on this spot." << endl;
+}
